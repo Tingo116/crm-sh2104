79ac9e8047e543344ddd941642aa29139c621186
jesse_global <997275742@qq.com>
2021-06-10 11:36:06 +0800

    2021-06-10 第一次提交CRM项目

---
+++ src/main/java/com/bjpowernode/crm/base/bean/Dog.java
@@ -0,0 +1,16 @@
+package com.bjpowernode.crm.base.bean;
+
+import lombok.Data;
+
+@Data
+public class Dog {
+
+    private String name;
+    private Float height;
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
